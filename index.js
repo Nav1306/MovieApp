9c1d0a918e2726d2170b774cadb6d5babfb012bb
Nav1306 <ce20b018@iittp.ac.in>
2024-01-03 15:03:51 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,149 @@
+const parentElement = document.querySelector(".main");
+const searchInput = document.querySelector(".input");
+const movieRatings = document.querySelector("#rating-select");
+
+
+let searchValue = "";
+let ratings = 0;
+let filteredArrOfMovies = [];
+
+const URL = "https://movies-app.prakashsakari.repl.co/api/movies";
+
+const getMovies = async (url) => {
+  try {
+    const { data } = await axios.get(url);
+    return data;
+  } catch (err) {}
+};
+
+let movies = await getMovies(URL);
+console.log(movies);
+
+const createElement = (element) => document.createElement(element);
+
+// fucntion to create movie cards
+
+const createMovieCard = (movies) => {
+  for (let movie of movies) {
+    // creating a parent container
+    const cardContainer = createElement("div");
+    cardContainer.classList.add("card", "shadow");
+
+    //creating image container
+    const imageContainer = createElement("div");
+    imageContainer.classList.add("card-image-container");
+
+    //creating card image
+    const imageEle = createElement("img");
+    imageEle.classList.add("card-image");
+    imageEle.setAttribute("src", movie.img_link);
+    imageEle.setAttribute("alt", movie.name);
+
+    imageContainer.appendChild(imageEle);
+
+    cardContainer.appendChild(imageContainer);
+
+    //creating card details container
+
+    const cardDetails = createElement("div");
+    cardDetails.classList.add("movie-details");
+
+    //creating card title
+    const titlEle = createElement("p");
+    titlEle.classList.add("title");
+    titlEle.innerText = movie.name;
+    cardDetails.appendChild(titlEle);
+
+    //creating card genre
+    const genreEle = createElement("p");
+    genreEle.classList.add("genre");
+    genreEle.innerText = `Genre: ${movie.genre}`;
+    cardDetails.appendChild(genreEle);
+
+    // ratings and length container
+    const movieRating = createElement("div");
+    movieRating.classList.add("ratings");
+
+    //star/rating component
+
+    const ratings = createElement("div");
+    ratings.classList.add("star-ratings");
+
+    //star icon
+    const starIcon = createElement("span");
+    starIcon.classList.add("material-icons-outlined");
+    starIcon.innerText = "star";
+    ratings.appendChild(starIcon);
+
+    // ratings
+    const ratingValue = createElement("span");
+    ratingValue.innerText = movie.imdb_rating;
+    ratings.appendChild(ratingValue);
+
+    movieRating.appendChild(ratings);
+
+    //length
+    const length = createElement("p");
+    length.innerText = `${movie.duration} mins`;
+
+    movieRating.appendChild(length);
+    cardDetails.appendChild(movieRating);
+    cardContainer.appendChild(cardDetails);
+
+    parentElement.appendChild(cardContainer);
+  }
+};
+
+
+function getFilteredData(){
+    filteredArrOfMovies =
+    searchValue?.length > 0 
+      ? movies.filter((movie) => 
+      searchValue === movie.name.toLowerCase() || 
+      searchValue === movie.director_name.toLowerCase() || 
+      movie.writter_name.toLowerCase().split(",").includes(searchValue)||
+      movie.cast_name.toLowerCase().split(",").includes(searchValue)
+      )
+      : movies;
+      if(ratings>0){
+        filteredArrOfMovies = searchValue?.length > 0 ? filteredArrOfMovies:movies;
+        filteredArrOfMovies = filteredArrOfMovies.filter(movie => movie.imdb_rating >= ratings);
+      }
+      return filteredArrOfMovies;
+}
+
+
+
+function handleSearch(event) {
+  searchValue = event.target.value.toLowerCase();
+  console.log(searchValue);
+      parentElement.innerHTML = "";
+      createMovieCard(filteredArrOfMovies);
+      console.log("Filtered Data:",filteredArrOfMovies);
+}
+
+function debounce(callback, delay) {
+    let timerId;
+    
+    return (...args) => {
+        clearTimeout(timerId);
+        timerId = setTimeout(() => {
+            callback(...args);
+        },delay);
+    };
+}
+
+function handleRatingSelector(event) {
+     ratings = event.target.value;
+     let filterByRating = getFilteredData();
+     parentElement.innerHTML="";
+     createMovieCard(ratings ? filterByRating:movies)
+}
+
+const debounceInput = debounce(handleSearch,1000);
+
+searchInput.addEventListener("keyup", debounceInput);
+
+movieRatings.addEventListener("change",handleRatingSelector)
+
+createMovieCard(movies);
